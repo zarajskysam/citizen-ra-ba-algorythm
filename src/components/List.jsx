bad238e2cd4a63f02262fbcc0dedf9f996ffbd6e
zarajskysam <zarajskysam92@gmail.com>
2021-04-14 11:12:14 +0300

    last commit

---
+++ src/components/List.jsx
@@ -0,0 +1,46 @@
+import { nanoid } from 'nanoid';
+import React from 'react';
+import { useEffect, useState } from 'react';
+import ElementTree from './ElementTree';
+
+export default function List() {
+
+    
+    let [ citizens, setCitisens ] = useState([]);
+    
+// Обращаемя к серверу за получением данных
+    useEffect(() => {
+      async function getFetch(url, func) {
+        try {
+          const response = await fetch(url);
+          if (!response.ok) {
+            throw new Error(response.message);
+          }
+  
+          const responseArray = await response.json();
+          func(responseArray);
+        } catch (e) {
+          console.error(e);
+        }
+      } 
+
+      getFetch('http://localhost:7777/citizens', setCitisens); 
+    }, [])
+
+
+    
+   
+  
+  
+
+ 
+
+  
+    return (
+        <div className='citizen'>
+        {citizens.map(item => (
+          <ElementTree key={nanoid()} data={item}/>
+        ))}
+        </div>
+    )
+}
